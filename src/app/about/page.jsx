33e16ba8975e48f38db7b44feecb883f0837dd11
mahmud <rajabovmahmudmm@gmail.com>
2025-02-24 20:40:57 +0500

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+    <p>
+      Welcome to our movie database website! we are a team of possionate movie enthusiasts who have come together to create a one-stop destination 
+      for all your movie-related needs
+    </p>
+    </div>
+  )
+}
