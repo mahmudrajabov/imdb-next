2d9502c7d7e80281037b12e0978beb3aa9838764
mahmud <rajabovmahmudmm@gmail.com>
2025-02-24 20:16:00 +0500

    add dark mood

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] })
+import Providers from "./Providers";
 
 export const metadata = {
   title: "IMDb clone",
@@ -21,9 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Header />
-        {children}
+        className={inter.className}>
+      <Providers>
+      <Header />
+      {children}
+      </Providers>
       </body>
     </html>
   );
