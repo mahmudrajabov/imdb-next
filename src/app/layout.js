28f311bf8bb24106d597e81dc8e6c3f538e4ee7e
mahmud <rajabovmahmudmm@gmail.com>
2025-02-27 14:20:33 +0500

    create the navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "./components/Header";
 const inter = Inter({ subsets: ['latin'] })
 import Providers from "./Providers";
+import Navbar from "./components/Navbar";
 
 export const metadata = {
   title: "IMDb clone",
@@ -12,10 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={inter.className}>
+      <body className={inter.className}>
       <Providers>
       <Header />
+      <Navbar />
       {children}
       </Providers>
       </body>
